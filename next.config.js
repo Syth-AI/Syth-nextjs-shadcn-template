1d3d47aeed6665a0bd94b7a7c030f563dcb8fd99
Assem Alghaithi <alghaihtia@gmail.com>
2025-06-09 22:34:29 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,8 +7,13 @@ const nextConfig = {
   images: { unoptimized: true },
   experimental: {
     optimizePackageImports: ['lucide-react'],
+    // Force WASM SWC in environments that don't support native binaries
+    forceSwcTransforms: true,
   },
-  // Disable SWC minifier to avoid binary loading issues
+  // Force use of WASM SWC compiler
+  compiler: {
+    // This ensures we use the WASM version
+  },
   swcMinify: false,
 };
 
